fda108cab00724cd5b1bcbfff3ac2dcfd79bacc4
ERIC AUZENNE <ericauzenne@erics-macbook-pro-2.local>
2018-11-30 22:39:45 -0800

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by ERIC AUZENNE on 11/30/18.
+//  Copyright © 2018 ERIC AUZENNE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
